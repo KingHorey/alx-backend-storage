b7b4b7efdd9e998b2f1f3ba0e284e3f1cf608082
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-13 16:02:31 +0100

    remove DEFINER

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,3 +1,3 @@
 -- Trigger event to occur on update
 
-CREATE root@'localhost' TRIGGER update_stock AFTER UPDATE ON users for EACH ROW set @quantity = OLD.quantity - NEW.quantity
+CREATE TRIGGER update_stock AFTER UPDATE ON users for EACH ROW set @quantity = OLD.quantity - NEW.quantity
