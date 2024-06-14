775f7933185ba0f735076315806c571bd63ffcca
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-14 02:17:10 +0100

    add trigger command once inserted into orders table

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,3 +1,11 @@
 -- Trigger event to occur on update
 
-CREATE TRIGGER update_stock AFTER UPDATE ON users for EACH ROW set @quantity = OLD.quantity - NEW.quantity
+DELIMITER //
+CREATE TRIGGER update_stock
+AFTER INSERT ON orders FOR EACH ROW
+BEGIN
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE items.name = NEW.item_name;
+END
+//
