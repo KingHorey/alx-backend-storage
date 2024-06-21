5720fa162a6d1b3f28a0f69caa389ec77092efcf
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-21 10:26:04 +0100

    Used Union for store annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """ import redis for daster caching
     import uuid4 for generating unique keys
     import json for converting data to json format
@@ -7,19 +6,19 @@
 """
 import redis
 from uuid import uuid4
-from typing import Any
+from typing import Any, Union
 import json
 
 
 class Cache:
     """ class to create a redis instance and cache data
     """
-    def __init__(self):
+    def __init__(self) -> None:
         """ create an instance of redis """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str | bytes, int, float]) -> str:
         """ store data in redis """
         key = str(uuid4())
         self._redis.set(key, json.dumps(data))
